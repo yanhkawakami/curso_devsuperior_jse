c577194466e643d4dfe0d7a5695e504d39ce8ace
Yan Hideki Kawakami <yanhkawakami@gmail.com>
2025-12-01 18:13:03 -0300

    add: adicao dos testes do resource para o put

--- dscatalog/backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ dscatalog/backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -17,8 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
