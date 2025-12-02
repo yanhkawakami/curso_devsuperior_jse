26eb586be893faaf5ced107874e5a70c26f230e3
Yan Hideki Kawakami <YanHidekiKawakami@MacBook-Pro-de-Yan.local>
2025-12-02 16:53:37 -0300

    add: adicao dos testes de integracao

--- dscatalog/backend/src/test/java/com/devsuperior/dscatalog/services/ProducServiceIntegrationTest.java
+++ dscatalog/backend/src/test/java/com/devsuperior/dscatalog/services/ProducServiceIntegrationTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
