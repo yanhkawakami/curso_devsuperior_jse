8821bd0c1ae54af1be90e90ac170e263643d52bc
Yan Hideki Kawakami <YanHidekiKawakami@MacBook-Pro-de-Yan.local>
2025-12-03 07:38:33 -0300

    add: adicao dos testes de integração

--- dscatalog/backend/src/test/java/com/devsuperior/dscatalog/services/ProducServiceIntegrationTest.java
+++ dscatalog/backend/src/test/java/com/devsuperior/dscatalog/services/ProducServiceIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -57,6 +58,25 @@ public class ProducServiceIntegrationTest {
         Assertions.assertEquals(10, result.getSize());
         Assertions.assertEquals(countTotalProducts, result.getTotalElements());
 
+    }
+
+    @Test
+    public void findAllShouldReturnEmptyPageWhenPageDoesNotExist() {
+        PageRequest pageRequest = PageRequest.of(50, 10);
+        Page<ProductDTO> result = service.findAll(pageRequest);
+
+            Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+        Page<ProductDTO> result = service.findAll(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
     }
 
 }
