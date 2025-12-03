9e5d12580e9643d368a39e22c8c261e1e3551109
Yan Hideki Kawakami <YanHidekiKawakami@MacBook-Pro-de-Yan.local>
2025-12-03 07:56:45 -0300

    add: adicao dos testes de integração

---
+++ dscatalog/backend/src/test/java/com/devsuperior/dscatalog/resources/ProducResourceIntegrationTest.java
@@ -0,0 +1,99 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.factory.ProductFactory;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProducResourceIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private PageImpl<ProductDTO> page;
+    private ProductDTO productDto;
+    private Long existingId;
+    private Long nonExistingId;
+    private Long dependentId;
+    private String jsonBody;
+    private int countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        productDto = ProductFactory.createProductDTO();
+        page = new PageImpl<>(List.of(productDto));
+        existingId = 1L;
+        nonExistingId = 1000L;
+        dependentId = 3L;
+        countTotalProducts = 25;
+        jsonBody = objectMapper.writeValueAsString(productDto);
+    }
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception{
+        ResultActions result =
+                mockMvc.perform(get("/products?page=0&size=12&sort=name,asc")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+        result.andExpect(jsonPath("$.content").exists());
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+        result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+
+        String expectedName = productDto.getName();
+        String expectedDescription = productDto.getDescription();
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", existingId, productDto)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(existingId));
+        result.andExpect(jsonPath("$.name").value(expectedName));
+        result.andExpect(jsonPath("$.description").value(expectedDescription));
+    }
+
+    @Test
+    public void putShouldReturnNotFoundWhenDoesNotIdExists() throws Exception {
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", nonExistingId, productDto)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+    }
+
+}
