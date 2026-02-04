6a29cba56361b39fb139565d34000e0e31184d8e
Yan Hideki Kawakami <yanhkawakami@gmail.com>
2026-02-04 15:07:23 -0300

    add: adicao do dscommerce e os testes iniciais

---
+++ 5_cobertura_jacoco/exercicios-aula/exercicio-jacoco-dscommerce/src/test/java/com/devsuperior/dscommerce/services/CategoryServiceTests.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.repositories.CategoryRepository;
+import com.devsuperior.dscommerce.tests.CategoryFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@ExtendWith(SpringExtension.class)
+public class CategoryServiceTests {
+
+    @InjectMocks
+    private CategoryService service;
+
+    @Mock
+    private CategoryRepository repository;
+
+    private Category category;
+    private List<Category> list;
+
+    @BeforeEach
+    void setUp(){
+        category = CategoryFactory.createCategory();
+
+        list = new ArrayList<>();
+        list.add(category);
+
+        Mockito.when(repository.findAll()).thenReturn(list);
+    }
+
+    @Test
+    public void findAllShouldReturnListCategoryDTO() {
+        List<CategoryDTO> result = service.findAll();
+
+        Assertions.assertEquals(1, list.size());
+        Assertions.assertEquals("Games", list.getFirst().getName());
+    }
+
+
+}
