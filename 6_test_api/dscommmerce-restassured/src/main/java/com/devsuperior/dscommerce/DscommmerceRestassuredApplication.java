a77ad424425de2d29cc658a3c71bc0fd87bd26ee
Yan Hideki Kawakami <yanhkawakami@gmail.com>
2026-04-28 17:34:00 -0300

    add: adicao do rest assured para testes do controlador

---
+++ 6_test_api/dscommmerce-restassured/src/main/java/com/devsuperior/dscommerce/DscommmerceRestassuredApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommmerceRestassuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommmerceRestassuredApplication.class, args);
+	}
+
+}
---
+++ 6_test_api/dscommmerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+public class ProductControllerRA {
+
+    private Long existingProductId, nonExistingProductId;
+
+    @BeforeEach
+    public void setUp(){
+        baseURI = "http://localhost:8080";
+    }
+
+    @Test
+    public void findByIdShouldReturnProductWhenIdExists(){
+        existingProductId = 2L;
+
+        given()
+            .get("/products/{id}", existingProductId)
+        .then()
+            .statusCode(200)
+                .body("id", is(2))
+                .body("name", equalTo("Smart TV"))
+                .body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
+                .body("price", is(2190.0F))
+                .body("categories.id", hasItems(2, 3))
+                .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+}
